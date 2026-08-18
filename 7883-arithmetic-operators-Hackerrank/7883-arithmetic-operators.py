bb6440f6484f18f964ea8c9c54a15583d09bdea3
jaladysravani22 <sravaniiisravs08@gmail.com>
2026-08-18 21:59:35 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 7883-arithmetic-operators-Hackerrank/7883-arithmetic-operators.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
