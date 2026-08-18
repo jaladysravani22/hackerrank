5ccb8b91481349413109462533c0712020c4d3dd
jaladysravani22 <sravaniiisravs08@gmail.com>
2026-08-18 21:58:54 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 9693-the-minion-game-Hackerrank/9693-the-minion-game.py
@@ -0,0 +1,8 @@
+def minion_game(string):
+    # your code goes here
+
+if __name__ == '__main__':
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
